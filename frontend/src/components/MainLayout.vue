593e134db91bf8d46b1d595701c38c276ca320e8
Angelo Lima Dias <angelolimadias@gmail.com>
2025-10-25 01:27:58 -0300

    ajuste

--- frontend/src/components/MainLayout.vue
+++ frontend/src/components/MainLayout.vue
@@ -2,6 +2,7 @@
 import { onMounted } from 'vue';
 import AppHeader from './AppHeader.vue';
 import AppFooter from './AppFooter.vue';
+import BottomNav from './BottomNav.vue';
 import { useThemeStore } from '../stores/theme';
 
 const themeStore = useThemeStore();
@@ -14,12 +15,13 @@ onMounted(() => {
 <template>
   <div class="flex flex-col min-h-screen">
     <AppHeader />
-    
-    <main class="flex-1">
+
+    <main class="flex-1 pb-20 lg:pb-0">
       <slot />
     </main>
-    
+
     <AppFooter />
+    <BottomNav />
   </div>
 </template>
 
